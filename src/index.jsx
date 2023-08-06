7a8cbad0b27811cd75cf2cf7918f782385152abf
BuildTools <unconfigured@null.spigotmc.org>
2023-08-06 19:52:03 -0400

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "./index.css"
+
+function Test() {
+    return (
+        <h1>Tesng</h1>
+    )
+}
+
+ReactDOM.render(<Test />, document.getElementById("root"))
